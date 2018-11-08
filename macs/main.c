9e788489a7914f397aaa9041ec1cce02ff3f9825
Tim Ludwig <ludwig.tim@t-online.de>
2018-11-09 00:03:33 +0100

    Move cmd execution to separate file

--- macs/main.c
+++ macs/main.c
@@ -8,88 +8,10 @@
 #include <avr/io.h>
 #include <stdlib.h>
 #include <util/delay.h>
+#include "units/execution.h"
 #include "serial/serial.h"
 #include "torquer/torquer.h"
 #include "parser.h"
-
-extern uint8_t fastmap(uint8_t original);
-
-static struct setpoint_t lastExecutedCmd;
-
-void executeCommand(struct setpoint_t *cmd) {
-	
-	lastExecutedCmd.x = cmd->x;
-	lastExecutedCmd.y = cmd->y;
-	lastExecutedCmd.z = cmd->z;
-	/* scale inputs */
-	int16_t xCycle = cmd->x;
-	int16_t yCycle = cmd->y;
-	int16_t zCycle = cmd->z;
-	
-	if(xCycle < 0)
-	{
-		xCycle *= -1;
-		xTorquerSetDutyCycle(fastmap(xCycle));
-		xTorquerEnableChannelA();
-	}
-	else if(xCycle == 0)
-	{
-		xTorquerDisable();
-	}
-	else
-	{
-		xTorquerSetDutyCycle(fastmap(xCycle));
-		xTorquerEnableChannelB();
-	}
-	
-	if(yCycle < 0)
-	{
-		yCycle *= -1;
-		yTorquerSetDutyCycle(fastmap(yCycle));
-		yTorquerEnableChannelA();
-	}
-	else if(yCycle == 0)
-	{
-		yTorquerDisable();
-	}
-	else
-	{
-		yTorquerSetDutyCycle(fastmap(yCycle));
-		yTorquerEnableChannelB();
-	}
-
-	if(zCycle < 0)
-	{
-		zCycle *= -1;
-		zTorquerSetDutyCycle(fastmap(zCycle));
-		zTorquerEnableChannelA();
-	} 
-	else if(zCycle == 0)
-	{
-		zTorquerDisable();
-	}
-	else
-	{
-		zTorquerSetDutyCycle(fastmap(zCycle));
-		zTorquerEnableChannelB();
-	}
-}
-
-
-void printCommand(struct setpoint_t *cmd)
-{
-	serial_puts("NEW TARGET: [");
-	char buffer[7];
-	itoa(cmd->x, buffer, 10);
-	serial_puts(buffer);
-	serial_putc(' ');
-	itoa(cmd->y, buffer, 10);
-	serial_puts(buffer);
-	serial_putc(' ');
-	itoa(cmd->z, buffer, 10);
-	serial_puts(buffer);
-	serial_puts("]\r");
-}
 
 int main(void)
 {
---
+++ macs/units/execution.h
@@ -0,0 +1,80 @@
+/*
+ * execution.h
+ *
+ * Created: 08.11.2018 23:59:15
+ *  Author: Tim
+ */ 
+
+
+#ifndef EXECUTION_H_
+#define EXECUTION_H_
+
+#include <avr/io.h>
+#include "../parser.h"
+#include "../torquer/torquer.h"
+
+
+extern uint8_t fastmap(uint8_t original);
+
+static struct setpoint_t lastExecutedCmd;
+
+
+void executeCommand(struct setpoint_t *cmd) {
+	
+	lastExecutedCmd.x = cmd->x;
+	lastExecutedCmd.y = cmd->y;
+	lastExecutedCmd.z = cmd->z;
+	/* scale inputs */
+	int16_t xCycle = cmd->x;
+	int16_t yCycle = cmd->y;
+	int16_t zCycle = cmd->z;
+	
+	if(xCycle < 0)
+	{
+		xCycle *= -1;
+		xTorquerSetDutyCycle(fastmap(xCycle));
+		xTorquerEnableChannelA();
+	}
+	else if(xCycle == 0)
+	{
+		xTorquerDisable();
+	}
+	else
+	{
+		xTorquerSetDutyCycle(fastmap(xCycle));
+		xTorquerEnableChannelB();
+	}
+	
+	if(yCycle < 0)
+	{
+		yCycle *= -1;
+		yTorquerSetDutyCycle(fastmap(yCycle));
+		yTorquerEnableChannelA();
+	}
+	else if(yCycle == 0)
+	{
+		yTorquerDisable();
+	}
+	else
+	{
+		yTorquerSetDutyCycle(fastmap(yCycle));
+		yTorquerEnableChannelB();
+	}
+
+	if(zCycle < 0)
+	{
+		zCycle *= -1;
+		zTorquerSetDutyCycle(fastmap(zCycle));
+		zTorquerEnableChannelA();
+	}
+	else if(zCycle == 0)
+	{
+		zTorquerDisable();
+	}
+	else
+	{
+		zTorquerSetDutyCycle(fastmap(zCycle));
+		zTorquerEnableChannelB();
+	}
+}
+#endif /* EXECUTION_H_ */
